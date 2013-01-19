95a590bdf043cd43aa8e8156f0c9684b2a4a84a0
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2013-01-19 12:37:53 +0000

    remove _dp

--- bpsd_constants.f90
+++ bpsd_constants.f90
@@ -2,22 +2,22 @@ module bpsd_constants
   use bpsd_kinds
   implicit none
 
-  real(rkind),parameter :: ZERO = 0.0_dp
-  real(rkind),parameter :: HALF = 0.5_dp
-  real(rkind),parameter :: ONE  = 1.0_dp
-  real(rkind),parameter :: TWO  = 2.0_dp
+  real(rkind),parameter :: ZERO = 0.0_rkind
+  real(rkind),parameter :: HALF = 0.5_rkind
+  real(rkind),parameter :: ONE  = 1.0_rkind
+  real(rkind),parameter :: TWO  = 2.0_rkind
 
-  real(rkind),parameter :: PI   = 3.14159265358979323846_dp
+  real(rkind),parameter :: PI   = 3.14159265358979323846_rkind
   real(rkind),parameter :: TWOPI= PI+PI
 
   ! ***** Physical constants based on CODATA 2006 *****
 
-  real(rkind),parameter :: AEE  = 1.602176487E-19_dp ! elementary charge
-  real(rkind),parameter :: AME  = 9.10938215E-31_dp  ! electron mass
-  real(rkind),parameter :: AMP  = 1.672621637E-27_dp ! proton mass
-  real(rkind),parameter :: VC   = 2.99792458E8_dp  ! speed of light
-  real(rkind),parameter :: RMU0 = 4.E-7_dp*PI      ! permeability
-  real(rkind),parameter :: EPS0 = ONE/(VC*VC*RMU0) ! permittivity
+  real(rkind),parameter :: AEE  = 1.602176487E-19_rkind ! elementary charge
+  real(rkind),parameter :: AME  = 9.10938215E-31_rkind  ! electron mass
+  real(rkind),parameter :: AMP  = 1.672621637E-27_rkind ! proton mass
+  real(rkind),parameter :: VC   = 2.99792458E8_rkind    ! speed of light
+  real(rkind),parameter :: RMU0 = 4.E-7_rkind*PI        ! permeability
+  real(rkind),parameter :: EPS0 = ONE/(VC*VC*RMU0)      ! permittivity
 
-  complex(rkind),parameter :: CI   = (0.0_dp,1.0_dp)
+  complex(rkind),parameter :: CI   = (0.0_rkind,1.0_rkind)
 end module bpsd_constants
--- bpsd_kinds.f90
+++ bpsd_kinds.f90
@@ -2,5 +2,4 @@ module bpsd_kinds
   implicit none
   integer, parameter :: rkind=selected_real_kind(12,100)
   integer, parameter :: ikind=selected_int_kind(8)
-  integer, parameter :: dp=rkind
 end module bpsd_kinds
