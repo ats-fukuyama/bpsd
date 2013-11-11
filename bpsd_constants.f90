fd6513d17e6d17570378842a736537b153f5294d
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2013-11-11 13:30:16 +0000

    update bpsd_constants

--- bpsd_constants.f90
+++ bpsd_constants.f90
@@ -12,11 +12,20 @@ module bpsd_constants
 
   ! ***** Physical constants based on CODATA 2006 *****
 
-  real(rkind),parameter :: AEE  = 1.602176487E-19_dp ! elementary charge
-  real(rkind),parameter :: AME  = 9.10938215E-31_dp  ! electron mass
-  real(rkind),parameter :: AMP  = 1.672621637E-27_dp ! proton mass
-  real(rkind),parameter :: VC   = 2.99792458E8_dp    ! speed of light
-  real(rkind),parameter :: RMU0 = 4.E-7_dp*PI        ! permeability
+!  real(rkind),parameter :: AEE  = 1.602176487E-19_dp ! elementary charge
+!  real(rkind),parameter :: AME  = 9.10938215E-31_dp  ! electron mass
+!  real(rkind),parameter :: AMP  = 1.672621637E-27_dp ! proton mass
+!  real(rkind),parameter :: VC   = 2.99792458E8_dp    ! speed of light
+!  real(rkind),parameter :: RMU0 = 4.E-7_dp*PI        ! permeability
+!  real(rkind),parameter :: EPS0 = ONE/(VC*VC*RMU0)   ! permittivity
+
+  ! ***** Physical constants based on CODATA 2010 *****
+
+  real(rkind),parameter :: AEE  = 1.602176565E-19_dp ! elementary charge [C]
+  real(rkind),parameter :: AME  = 9.10938291E-31_dp  ! electron mass     [kg]
+  real(rkind),parameter :: AMP  = 1.672621777E-27_dp ! proton mass       [kg]
+  real(rkind),parameter :: VC   = 2.99792458E8_dp    ! speed of light    [m/s]
+  real(rkind),parameter :: RMU0 = 4.E-7_dp*PI        ! permeability      [H/m]
   real(rkind),parameter :: EPS0 = ONE/(VC*VC*RMU0)   ! permittivity
 
   complex(rkind),parameter :: CI   = (0.0_dp,1.0_dp)
