cf1ad03bcd81110c8c5e70a88c4f61a6de32c7c0
Atsushi Fukuyama <fukuyama@afportal.net>
2020-08-30 21:37:31 +0900

    bpsd_kinds: sp (single precision) added

--- bpsd_kinds.f90
+++ bpsd_kinds.f90
@@ -4,5 +4,7 @@ module bpsd_kinds
   implicit none
   integer, parameter :: rkind=selected_real_kind(12,100)
   integer, parameter :: ikind=selected_int_kind(8)
+!  integer, parameter :: dp=selected_real_kind(15)
   integer, parameter :: dp=rkind
+  integer, parameter :: sp=selected_real_kind(6)
 end module bpsd_kinds
